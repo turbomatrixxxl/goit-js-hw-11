83722a99d3d3ed4d9283538287454033b4afe9b4
turbomatrixxxl <turbomatrixxxl@gmail.com>
2024-05-15 13:28:19 +0000

    Deploying to gh-pages from @ turbomatrixxxl/goit-js-hw-11@2919eee020bdde5877e18cc2a4c2ed793040a6ad 🚀

---
+++ index.c50920dc.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#search-form"),t=document.querySelector("input"),n=document.querySelector("button");n.disabled=!0,n.addEventListener("click",(function(e){e.preventDefault()})),e.addEventListener("input",(function(e){e.preventDefault(),n.disabled=!1,console.log(t.value)}))}();
+//# sourceMappingURL=index.c50920dc.js.map
