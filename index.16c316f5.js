a38ce87c40daa16e985c605192f8245db4c6f92b
turbomatrixxxl <turbomatrixxxl@gmail.com>
2024-05-16 01:24:40 +0000

    Deploying to gh-pages from @ turbomatrixxxl/goit-js-hw-11@1be5313b71b091885d2ac1cf4ab977afae14089f 🚀

--- index.16c316f5.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#search-form"),t=document.querySelector("input"),n=document.querySelector("button");n.disabled=!0,n.addEventListener("click",(e=>{e.preventDefault()})),e.addEventListener("input",(e=>{e.preventDefault(),n.disabled=!1,console.log(t.value)}));
-//# sourceMappingURL=index.16c316f5.js.map
